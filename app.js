9490312447f97b0c7886be5599e92048b2988a5d
Eric William Dolan <eisforinnovate@gmail.com>
2015-09-20 10:21:01 -0400

    new new

--- app.js
+++ app.js
@@ -13,3 +13,22 @@ app.use(express.static(__dirname + '/web-app'));
 //start the server on the port
 app.listen(process.env.PORT || 8080);
 console.log("Express server listening on port %d", process.env.PORT || 8080);
+
+<script>
+//firebase login with facebook
+    //var ref = new Firebase('https://boiling-fire-2567.firebaseio.com');
+    $rootScope.auth = loginService.getAuth();
+
+    $scope.logout = function(){
+        $rootScope.auth.$logout();
+        $rootScope.auth.user = null;
+        $location.path('/login');
+    };
+
+    $scope.login = function(provider){
+        $rootScope.auth.$login(provider);
+
+    }
+
+});
+</script>
